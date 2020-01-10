098bdfa4aecd195d0c6729582f8abf4e787a33d1
WacuMbugua <58767172+WacuMbugua@users.noreply.github.com>
2020-01-11 00:49:24 +0300

    VueAppv2

--- App.vue
+++ App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <trial />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import trial from './components/trial.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    trial
   }
 }
 </script>
